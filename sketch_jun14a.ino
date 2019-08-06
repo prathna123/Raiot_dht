3a776ea5daaa6db02e3000d3430fbc1bb382939e
prathna123 <33526604+prathna123@users.noreply.github.com>
2019-08-06 18:28:17 +0530

    Create sketch_jun14a.ino

---
+++ sketch_jun14a.ino
@@ -0,0 +1,51 @@
+#include <MQTT.h>
+#include <Bridge.h>
+#include <BridgeClient.h>
+#include <MQTTClient.h>
+
+BridgeClient net;
+MQTTClient client;
+
+unsigned long lastMillis = 0;
+
+void setup() {
+  Bridge.begin();
+  Serial.begin(9600);
+  client.begin("broker.shiftr.io", net);
+
+  connect();
+}
+
+void connect() {
+  Serial.print("connecting...");
+  while (!client.connect("arduino", "try", "try")) {
+    Serial.print(".");
+  }
+
+  Serial.println("\nconnected!");
+
+  client.subscribe("/example");
+  // client.unsubscribe("/example");
+}
+
+void loop() {
+  client.loop();
+
+  if(!client.connected()) {
+    connect();
+  }
+
+  // publish a message roughly every second.
+  if(millis() - lastMillis > 1000) {
+    lastMillis = millis();
+    client.publish("/hello", "world");
+  }
+}
+
+void messageReceived(String topic, String payload, char * bytes, unsigned int length) {
+  Serial.print("incoming: ");
+  Serial.print(topic);
+  Serial.print(" - ");
+  Serial.print(payload);
+  Serial.println();
+}
